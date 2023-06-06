d7adfdb1b497c4d4344ff5ba83e30687c1339e4a
stvnlee890 <stvnlee890@gmail.com>
2023-06-06 15:59:28 -0700

    Work on case studies section and work on dummy data

---
+++ src/utils/projects/caseStudies.js
@@ -0,0 +1,35 @@
+export const caseStudies = [
+  {
+    id: "001",
+    name: "Carousel experiment",
+    year:'2023',
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Nihil soluta delectus repellendus ducimus recusandae dolores placeat sint 
+        quidem corrupti, facere libero quasi! Magni quidem esse voluptates cupiditate 
+        assumenda at asperiores!`,
+    githubLink: 'somelink',
+
+  },
+  {
+    id: "002",
+    name: "Nav experiment",
+    year:'2023',
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Nihil soluta delectus repellendus ducimus recusandae dolores placeat sint 
+        quidem corrupti, facere libero quasi! Magni quidem esse voluptates cupiditate 
+        assumenda at asperiores!`,
+    githubLink: 'somelink',
+
+  },
+  {
+    id: "003",
+    name: "Shaders experiment",
+    year:'2023',
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Nihil soluta delectus repellendus ducimus recusandae dolores placeat sint 
+        quidem corrupti, facere libero quasi! Magni quidem esse voluptates cupiditate 
+        assumenda at asperiores!`,
+    githubLink: 'somelink',
+
+  },
+];
