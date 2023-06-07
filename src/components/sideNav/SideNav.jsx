4635d1cc2fa1e6a570a63818c1612be857764b33
stvnlee890 <stvnlee890@gmail.com>
2023-06-07 14:38:20 -0700

    Able to add animation to side nav

--- src/components/sideNav/SideNav.jsx
+++ src/components/sideNav/SideNav.jsx
@@ -1,48 +1,72 @@
-import "./sideNav.css"
-import { useEffect, useState } from "react"
+import "./sideNav.css";
+import { useEffect, useState, useRef, useLayoutEffect } from "react";
+
+import { sideNav, reverseSideNav } from "../../utils/animations/animations";
 
 export default function SideNav({ appRef }) {
-
-const [navDisp, setNavDisp] = useState("")
+  const [navDisp, setNavDisp] = useState("");
+  const [newVal, setNewVal] = useState(false);
+  const sideNavRef = useRef(null);
 
   useEffect(() => {
     const handleScroll = () => {
-      const yVal = window.pageYOffset
-      if(yVal > 70) {
-        setNavDisp("unhide")
-      } else {
-        setNavDisp("")
+      const yVal = window.pageYOffset;
+      if (yVal > 62.5) {
+        setNavDisp("unhide");
+        setNewVal(true);
+      } else if (yVal <= 20) {
+        setNavDisp("");
+        setNewVal(false)
       }
-    }
-    window.addEventListener('scroll', handleScroll)
+    };
+    window.addEventListener("scroll", handleScroll);
 
+    // cleanup function
     return () => {
-      window.removeEventListener('scroll', handleScroll)
+      // removes when component unmounts
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useLayoutEffect(() => {
+    console.log(newVal);
+    if (newVal) {
+      sideNav(sideNavRef.current);
+    } else {
+      // sideNav(sideNavRef.current, newVal)
+      reverseSideNav(sideNavRef.current)
     }
-  },[])
+  }, [newVal]);
 
   const handleClick = (e) => {
     let eleName;
     const target = e.target.id;
-    console.log(appRef.current.children[1])
     if (target === "p") {
       eleName = appRef.current.children[1].children[1];
     } else if (target === "a") {
-      eleName = appRef.current.children[1].children[0];
+      eleName = appRef.current;
     } else if (target === "cs") {
       eleName = appRef.current.children[1].children[2];
-    } 
+    }
     eleName.scrollIntoView({
       behavior: "smooth",
     });
   };
   return (
-    <div className={`side-nav-container ${navDisp}`}>
-      <ul>
-        <li onClick={handleClick} className="side-nav-list" id='a'>a</li>
-        <li onClick={handleClick} className="side-nav-list" id='p'>p</li>
-        <li onClick={handleClick} className="side-nav-list" id='cs'>c</li>
-        <li onClick={handleClick} className="side-nav-list" id='c'>c</li>
+    <div className="side-nav-container">
+      <ul ref={sideNavRef} className={`side-nav-wrapper`}>
+        <li onClick={handleClick} className="side-nav-list" id="a">
+          a
+        </li>
+        <li onClick={handleClick} className="side-nav-list" id="p">
+          p
+        </li>
+        <li onClick={handleClick} className="side-nav-list" id="cs">
+          c
+        </li>
+        <li onClick={handleClick} className="side-nav-list" id="c">
+          c
+        </li>
       </ul>
     </div>
   );
--- src/utils/animations/animations.js
+++ src/utils/animations/animations.js
@@ -1,53 +1,73 @@
-import { gsap, Power4 }from 'gsap'
-const tl = gsap.timeline()
+import { gsap, Power4 } from "gsap";
+const tl = gsap.timeline();
 
 export function navListAnimation(element) {
-    gsap.to(element, {
-        y: 0, 
-        ease: Power4.easeInOut,
-        duration: 1,
-        stagger: .25,
-    })
+  gsap.to(element, {
+    y: 0,
+    ease: Power4.easeInOut,
+    duration: 1,
+    stagger: 0.25,
+  });
 }
 
 export function aboutAnimation(element, element1) {
-    gsap.to(element, {
-        opacity: 1,
-        ease: Power4.easeInOut,
-        duration: 2
-    })
-    gsap.to(element1, {
-        y: 0,
-        ease: Power4.easeInOut,
-        duration: 2
-    })
+  gsap.to(element, {
+    opacity: 1,
+    ease: Power4.easeInOut,
+    duration: 2,
+  });
+  gsap.to(element1, {
+    y: 0,
+    ease: Power4.easeInOut,
+    duration: 2,
+  });
 }
 
 export function nameAnimation(element) {
-    gsap.to(element, {
-        opacity: 1,
-        duration: 1.5,
-        ease: Power4.easeInOut
-    })
+  gsap.to(element, {
+    opacity: 1,
+    duration: 1.5,
+    ease: Power4.easeInOut,
+  });
 }
 
 export function lineAnimation(line) {
-    gsap.to(line, {
-        width: '100%',
-        ease: Power4.easeIn,
-        duration: .5
-    })
+  gsap.to(line, {
+    width: "100%",
+    ease: Power4.easeIn,
+    duration: 0.5,
+  });
 }
 
 export function projectPageAnimation(title, container) {
-    tl.to(title, {
-        y: 0,
-        ease: Power4.easeInOut,
-        duration: 1
-    })
-    tl.to(container, {
-        opacity: 1,
-        ease: Power4.easeInOut,
-        duration: 1.5
-    })
+  tl.to(title, {
+    y: 0,
+    ease: Power4.easeInOut,
+    duration: 1,
+  });
+  tl.to(container, {
+    opacity: 1,
+    ease: Power4.easeInOut,
+    duration: 1.5,
+  });
+}
+
+export function sideNav(element) {
+  gsap.to(element.children, {
+    opacity: 1,
+    left: '1%',
+    stagger: 0.05,
+    ease: Power4.easeInOut,
+    duration: .9,
+  });
+}
+
+export function reverseSideNav(element) {
+  gsap.to(element.children, {
+    opacity: 0,
+    left: "-40px",
+    stagger: 0.05,
+    ease: Power4.easeInOut,
+    duration: .3,
+  });
 }
