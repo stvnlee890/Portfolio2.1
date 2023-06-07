ae61ba3735f9df1076165d1c5eeb0c128a38de20
stvnlee890 <stvnlee890@gmail.com>
2023-06-06 22:07:05 -0700

    Changing dummy data a bit

--- src/utils/projects/caseStudies.js
+++ src/utils/projects/caseStudies.js
@@ -1,32 +1,23 @@
 export const caseStudies = [
   {
     id: "001",
-    name: "Carousel experiment",
+    name: "Satellite",
     year: "2023",
-    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-        Nihil soluta delectus repellendus ducimus recusandae dolores placeat sint 
-        quidem corrupti, facere libero quasi! Magni quidem esse voluptates cupiditate 
-        assumenda at asperiores!`,
+    description: `Implement use of web workers to provide extra thread for etc`,
     githubLink: "somelink",
   },
   {
     id: "002",
-    name: "Nav experiment",
+    name: "Scrape",
     year: "2023",
-    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-        Nihil soluta delectus repellendus ducimus recusandae dolores placeat sint 
-        quidem corrupti, facere libero quasi! Magni quidem esse voluptates cupiditate 
-        assumenda at asperiores!`,
+    description: `Experiment with puppeteer to scrape data from sites and harvest it`,
     githubLink: "somelink",
   },
   {
     id: "003",
     name: "Shaders experiment",
     year: "2023",
-    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-        Nihil soluta delectus repellendus ducimus recusandae dolores placeat sint 
-        quidem corrupti, facere libero quasi! Magni quidem esse voluptates cupiditate 
-        assumenda at asperiores!`,
+    description: `Implement custom shaders for a better user experience`,
     githubLink: "somelink",
   },
 ];
