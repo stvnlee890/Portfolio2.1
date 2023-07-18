0ebd5c868353560580ae268f5d9a1a6239df5291
stvnlee890 <stvnlee890@gmail.com>
2023-07-18 14:39:33 -0700

    Add new project

--- src/utils/projects/currentProjects.js
+++ src/utils/projects/currentProjects.js
@@ -1,17 +1,13 @@
 export const projects = [
   {
     id: "001",
-    image: "/assets/example-project.webp",
+    image: "/assets/projectAssets/thinkTrivia/think-trivia.png",
     year: "2022",
-    name: "Music Discog",
-    builtWith: ["React", "AWS", "Express", "Tailwind", "CSS", "Node", "Tailwind"],
+    name: "Think Trivia",
+    builtWith: ["React", "TypeScript", "GSAP", "The Trivia API"],
     description:
-      `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-      Nihil soluta delectus repellendus ducimus 
-      recusandae dolores placeat sint quidem corrupti, 
-      facere libero quasi! Magni quidem esse voluptates cupiditate 
-      assumenda at asperiores!`,
-    link: "https://github.com/",
-    liveSite: "https://wikipedia.com/",
+      `Think Trivia is an engaging trivia quiz application that challenges your knowledge in various categories! Engage in endless brain-teasing fun as you tackle unpredictable questions that will keep you guessing!`,
+    link: "https://github.com/stvnlee890/Think-Trivia",
+    liveSite: "https://think-trivia.netlify.app/",
   },
 ];
